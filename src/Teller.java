20818a293b53fca4d095eef1741d487606bdc563
cj6046 <cdjones6046@gmail.com>
2021-09-15 19:01:14 -0400

    Updated Teller class
    Re-organizing the GUI code as I learn more efficient methods. Placed most GUI objects in constructor and calling constructor from main.

--- src/Teller.java
+++ src/Teller.java
@@ -7,80 +7,71 @@
  */
 import java.util.Scanner;
 import javax.swing.*;
+import java.awt.event.*;
 
-public class Teller {
-    
-    public static void main(String[] args) {
+public class Teller extends JFrame implements ActionListener {
+
+    // Instance variables
+    private JPanel panel;
+    private JLabel welcomeLabel;
+    private JLabel userLabel;
+    private JTextField userText;
+    private JLabel passLabel;
+    private JPasswordField passText;
+    private JButton login;
+    private JButton create;
+
+
+    public Teller() {
         // Attempt at GUI
-        JFrame frame = new JFrame("Jones Bank");
-        frame.setSize(350, 200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        super("Jones Bank");
+        this.setSize(350, 250);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel();
-        frame.add(panel);
+        panel = new JPanel();
+        this.add(panel);
         panel.setLayout(null);
 
-        JLabel userLabel = new JLabel("User ID");
-        userLabel.setBounds(10, 20, 80, 25);
+        welcomeLabel = new JLabel("Welcome to Jones Bank!");
+        welcomeLabel.setBounds(100, 10, 150, 25);
+        panel.add(welcomeLabel);
+
+        userLabel = new JLabel("User ID");
+        userLabel.setBounds(10, 40, 80, 25);
         panel.add(userLabel);
 
-        JTextField userText = new JTextField();
-        userText.setBounds(100, 20, 165, 25);
+        userText = new JTextField();
+        userText.setBounds(100, 40, 165, 25);
         panel.add(userText);
 
-        JLabel passLabel = new JLabel("Password");
-        passLabel.setBounds(10, 65, 80, 25);
+        passLabel = new JLabel("Password");
+        passLabel.setBounds(10, 85, 80, 25);
         panel.add(passLabel);
 
-        JPasswordField passText = new JPasswordField();
-        passText.setBounds(100, 65, 165, 25);
+        passText = new JPasswordField();
+        passText.setBounds(100, 85, 165, 25);
         panel.add(passText);
 
-        JButton login = new JButton("Login");
-        login.setBounds(140, 110, 80, 25);
+        login = new JButton("Login");
+        login.setBounds(140, 130, 80, 25);
         panel.add(login);
 
-/*
-        // Will first welcome user to bank
-        System.out.println("Welcome to Jones Bank!"
-                            + "\nPlease choose an option: "
-                            + "\n1) Login"
-                            + "\n2) Create Account"
-                            + "\n3) Close program.");
+        create = new JButton("Create an account");
+        create.setBounds(100, 170, 165, 25);
+        panel.add(create);
+    } // End of constructor
+
+    public static void main(String[] args) {
+        Teller teller = new Teller();
+        teller.setVisible(true);
+    } // End of main
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String testUserID = userText.getText();
+        String testUserPass = passText.getText();
         
-        Scanner in = new Scanner(System.in);
-        boolean done = false;
+        //Bank.authenticate(testUserID, testUserPass, Bank.findAccount(testUserID))
 
-        while(!done) {
-            // Then will sit at a menu
-            // 1) Login to account
-            // 2) Create account
-            int choice = in.nextInt();
-            switch(choice) {
-                case 1 : 
-                    // login will prompt them for a user ID associated with Account
-                    System.out.println("You chose login.");
-                    break;
-                case 2 :
-                    // create will ask them for a new user ID and create an Account
-                    System.out.println("You chose create account.");
-                    break;
-                case 3 :
-                    done = true;
-                    break;
-            }
-
-            // Once in account, they sit at another menu
-                // 1) Check balance
-                // 2) Deposit
-                // 3) Withdraw
-                // 4) Transfer
-                // 5) Exit
-
-        } // End of while(!done)
-
-        in.close(); */
-
-    } // End of main()
+    } // End of actionPerformed()
 } // End of Teller Class
